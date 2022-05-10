0636d740f06fed297624801b41619bc6b8625426
Cheri Hansen <coolcheri87@gmail.com>
2022-05-09 21:46:46 -0700

    got it working

--- cone_volume.py
+++ cone_volume.py
@@ -1,4 +1,4 @@
-""Compute and print the volume of a right circular cone."""
+"""Compute and print the volume of a right circular cone."""
 
 # Import the standard math module so that
 # math.pi can be used in this program.
@@ -10,7 +10,7 @@ def main():
     # the volume of an example cone.
     ex_radius = 2.8
     ex_height = 3.2
-    ex_vol = cone_volume()
+    ex_vol = cone_volume(ex_radius, ex_height)
 
     # Print several lines that describe this program.
     print("This program computes the volume of a right")
@@ -19,13 +19,14 @@ def main():
     print(f"then the volume is {ex_vol:.1f}")
     print()
 
-    # Get the radius and height of the cone from the user.
+    # Get the radius5
+    # 5 and height of the cone from the user.
     radius = float(input("Please enter the radius of the cone: "))
     height = float(input("Please enter the height of the cone: "))
 
     # Call the cone_volume function to compute the volume
     # for the radius and height that came from the user.
-    vol = cone_volume()
+    vol = cone_volume(radius, height)
 
     # Print the radius, height, and
     # volume for the user to see.

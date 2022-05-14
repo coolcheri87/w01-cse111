cb957b41ab6dc53d54206956a84d00ebf1800068
Cheri Hansen <72418856+coolcheri87@users.noreply.github.com>
2022-05-13 22:10:48 -0700

    Adding stuff

--- scene2.py
+++ scene2.py
@@ -10,6 +10,7 @@ from draw2d import \
     start_drawing, draw_line, draw_oval, draw_arc, \
     draw_rectangle, draw_polygon, draw_text, finish_drawing
 
+import random
 
 def main():
     # Width and height of the scene in pixels
@@ -23,10 +24,33 @@ def main():
     # Call your drawing functions such
     # as draw_sky and draw_ground here.
     draw_sky(canvas, scene_width, scene_height)
-    draw_pine_tree(canvas, 550, 150, 250)
-    draw_pine_tree(canvas, 200, 100, 200)
-    for x in range(100, 200, 100): 
-        draw_pine_tree(canvas, 400, 150, 180)
+
+    # draw the sun!
+    draw_sun(canvas, random.randint(5,795), random.randint(200,500))
+
+    # Draw a cloud.
+    for x in range(random.randint(5,8)):
+        x1 = random.randint(5,600)
+        y1 = random.randint(200,450)
+        dx = random.randint(50,300)
+        dy = random.randint(30,dx)
+        draw_cloud(canvas,x1,y1,x1+dx,y1+dy)
+
+    # draw lots of grasss
+    for y in reversed(range(10)):
+        for x in range(200):
+            draw_grass(canvas, x*4+y+random.randint(0,3), y*9+random.randint(0,4))
+
+    # draw four trees
+    treeX = [ 50, 175, 333, 700]
+    treeY = [ 22, 155, 130, 145]
+    for x in range(4): 
+        draw_pine_tree(canvas, treeX[x], treeY[x], 100+random.randint(25,100))
+
+    # draw houses
+    draw_house(canvas, random.randint(50, 250), random.randint(10, 30))
+    draw_house(canvas, random.randint(450, 650), random.randint(110, 130))
+    
     draw_grid(canvas, scene_width, scene_height, 50)
 
 
@@ -35,8 +59,22 @@ def main():
     finish_drawing(canvas)
 
 
-    # Define your functions such as
-    # draw_sky and draw_ground here.
+# draw a house
+def draw_house(canvas, leftX, bottomY):
+    for x in reversed(range(6)):
+        draw_oval(canvas, leftX+9+x*10, bottomY+152+x*10, leftX+25+x*15, bottomY+170+x*15, fill="darkgray")
+    draw_rectangle(canvas, leftX+10, bottomY+100, leftX+25, bottomY+155, fill="tan4") # chimney
+    draw_rectangle(canvas, leftX, bottomY, leftX+100, bottomY+100, fill="tan1") # building
+    draw_rectangle(canvas, leftX+15, bottomY, leftX+40, bottomY+60, fill="tan3") # door
+    draw_polygon(canvas, leftX-25, bottomY+100, leftX+125, bottomY+100, leftX+50, bottomY+150, fill="red") # root
+    draw_rectangle(canvas, leftX+60, bottomY+40, leftX+85, bottomY+85, fill="lightblue")
+
+# draw a little blade of grass
+def draw_grass(canvas, leftX, bottomY):
+    draw_polygon(canvas, leftX, bottomY+10, leftX+random.randint(2,4), bottomY+10, leftX+1, bottomY+15, outline="darkgreen", fill="green")
+    draw_rectangle(canvas, leftX, bottomY, leftX+random.randint(2,4), bottomY+10, outline="darkgreen", fill="green")
+
+# draw a pine tree
 def draw_pine_tree(canvas, center_x, bottom, height):
     # draw trunk of the tree.
     trunk_width = height / 10
@@ -46,10 +84,9 @@ def draw_pine_tree(canvas, center_x, bottom, height):
     right_trunk = center_x + trunk_width / 2
     trunk_top = bottom + trunk_height
     draw_rectangle(canvas, left_trunk, bottom_trunk, right_trunk, 
-    trunk_top, fill="tan4")
+    trunk_top, fill="tan2")
 
-
-    # draw skirt of the tree.
+    # draw leafy part of the tree.
     skirt_width = height / 2
     skirt_left = center_x - skirt_width / 2
     skirt_bottom = trunk_top
@@ -59,8 +96,7 @@ def draw_pine_tree(canvas, center_x, bottom, height):
     draw_polygon(canvas, skirt_left, skirt_bottom, peak_x,
     peak_y, skirt_right, skirt_bottom, fill= "forestGreen")
 
-
-
+# Draw a grid
 def draw_grid(canvas, width, height, interval):
     # draw verical lines
     label_y = 15
@@ -74,86 +110,22 @@ def draw_grid(canvas, width, height, interval):
         draw_line(canvas, label_x*2, y, width, y)
         draw_text(canvas, label_x, y, f"{y}") 
      
+# draw a fluffy white cloud!
 def draw_cloud(canvas, x0, y0, x1, y1):
     draw_oval(canvas, x0, y0, x1, y1, width=1, outline="white", fill="white")
 
+# draw mr. sunshine
+def draw_sun(canvas, x, y):
+    draw_oval(canvas, x-35, y-35, x+35, y+35, outline="yellow", fill="yellow")
+
+# draw the sky and ground...
 def draw_sky(canvas, scene_width, scene_height):
     """Draw the ground and all the objects on the ground."""
     draw_rectangle(canvas, 0, 0, scene_width, scene_height / 3, width = 0, fill="tan4")
     draw_rectangle(canvas, 0, scene_height / 3, scene_width, scene_height, width =0, fill="lightBlue")
 
-    # Draw a cloud.
-    x0 = 450
-    y0 = 500
-    x1 = 350
-    y1 = 400
-    draw_cloud(canvas, x0, y0, x1, y1)
-
-    # Draw a cloud.
-    x0 = 600
-    y0 = 350
-    x1 = 400
-    y1 = 250
-    draw_cloud(canvas, x0, y0, x1, y1)
-    
-    # Draw a cloud.
-    x0 = 100
-    y0 = 350
-    x1 = 400
-    y1 = 500
-    draw_cloud(canvas, x0, y0, x1, y1)
-
-    # Draw a pine tree.
-    tree_center_x = 170
-    tree_bottom = 100
-    tree_height = 200
-    draw_pine_tree(canvas, tree_center_x,
-            tree_bottom, tree_height)
-
-    # Draw another pine tree.
-    tree_center_x = 90
-    tree_bottom = 70
-    tree_height = 220
-    draw_pine_tree(canvas, tree_center_x,
-            tree_bottom, tree_height)
 
 
-def draw_pine_tree(canvas, center_x, bottom, height):
-    """Draw a single pine tree.
-    Parameters
-        canvas: The canvas where this function
-            will draw a pine tree.
-        center_x, bottom: The x and y location in pixels where
-            this function will draw the bottom of a pine tree.
-        height: The height in pixels of the pine tree that
-            this function will draw.
-    Return: nothing
-    """
-    trunk_width = height / 10
-    trunk_height = height / 8
-    trunk_left = center_x - trunk_width / 2
-    trunk_right = center_x + trunk_width / 2
-    trunk_top = bottom + trunk_height
-
-    # Draw the trunk of the pine tree.
-    draw_rectangle(canvas,
-            trunk_left, trunk_top, trunk_right, bottom,
-            outline="gray20", width=1, fill="tan3")
-
-    skirt_width = height / 2
-    skirt_height = height - trunk_height
-    skirt_left = center_x - skirt_width / 2
-    skirt_right = center_x + skirt_width / 2
-    skirt_top = bottom + height
-
-    # Draw the crown (also called skirt) of the pine tree.
-    draw_polygon(canvas, center_x, skirt_top,
-            skirt_right, trunk_top,
-            skirt_left, trunk_top,
-            outline="gray20", width=1, fill="dark green")
-  
-    # Draw the cloud in sky
-    #draw_oval(canvas, cloud_left, cloud+top, cloud_right, bottom, width=3, fill-"gray")
 
 # Call the main function so that
 # this program will start executing.

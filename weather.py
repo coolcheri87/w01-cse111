e70bb2d0313a5ea16afc435726ebfbf6f4d81983
Cheri Hansen <coolcheri87@gmail.com>
2022-05-16 23:50:33 -0700

    updated file

--- weather.py
+++ weather.py
@@ -1,3 +1,20 @@
+# test_weather.py
+from decimal import Rounded
+from weather import cels_from_fahr
+from pytest import approx
+import pytest
+
+def test_cels_from_fahr():
+    """Test the cels_from_fahr function by calling it and
+    comparing the values it returns to the expected values.
+    Notice this test function uses pytest.approx to compare
+    floating point numbers.
+    """
+    assert cels_from_fahr(-25) == approx(-31.66667)
+    assert cels_from_fahr(0) == approx(-17.77778)
+    assert cels_from_fahr(32) == approx(0)
+    assert cels_from_fahr(70) == approx(21.1111)
+
 def wind_chill(temperature, wind_speed):
     """ Computer and return the wind chill factor as defined by the US National Weather SErvice.
     Parameters
@@ -23,3 +40,22 @@ def heat_index(temperature, humidity):
     Return: The heat index in degrees Fahrenteit.
     """
 
+index = -42.379 \
+    + 2.049045253 * temperature \
+    + 10.1433127 * humidity \
+    - 0.22475541 * temperature * humidity \
+    - 0.00683783 * temperature**2 \
+    - 0.05481717 * humidity**2 \
+    + 0.00122874 * temperature**2 * humidity**2 
+    rounded * round(index, 1)
+    return rounded
+
+def celx_from_fahr(fahr):
+    """Convert a temperature in fahrenheit to 
+    Celsius and return the Calisius temperature
+    """
+    
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])

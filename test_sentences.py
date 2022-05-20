50ae8ca6b08ccc63f2b0fe9a3978a4501b2ca212
Cheri Hansen <coolcheri87@gmail.com>
2022-05-19 20:53:35 -0700

    update file

---
+++ test_sentences.py
@@ -0,0 +1,130 @@
+# Cheri Hansen
+from sentences import get_determiner, get_noun, get_verb
+import random
+import pytest
+
+# Create a list of strings and assign
+# the list to a variable named words.
+words = ["boy", "girl", "cat", "dog", "bird", "house"]
+
+# Call the random.choice function which will choose
+# one string from the words list. Store the chosen
+# string in a variable named word.
+word = random.choice(words)
+
+
+def get_determiner(quantity):
+    """Return a randomly chosen determiner. A determiner is
+    a word like "the", "a", "one", "two", "some", "many".
+    If quantity == 1, this function will return either "a",
+    "one", or "the". Otherwise this function will return
+    either "two", "some", "many", or "the".
+
+    Parameter
+        quantity: an integer.
+            If quantity == 1, this function will return a
+            determiner for a single noun. Otherwise this
+            function will return a determiner for a plural
+            noun.
+    Return: a randomly chosen determiner.
+    """
+    if quantity == 1:
+        words = ["a", "one", "the"]
+    else:
+        words = ["two", "some", "many", "the"]
+
+    determ = get_determiner(1)
+        ["a", "one", "the"]
+
+def get_noun(quantity):
+     """Return a randomly chosen noun.
+    If quantity == 1, this function will
+    return one of these ten single nouns:
+        "bird", "boy", "car", "cat", "child",
+        "dog", "girl", "man", "rabbit", "woman"
+    Otherwise, this function will return one of
+    these ten plural nouns:
+        "birds", "boys", "cars", "cats", "children",
+        "dogs", "girls", "men", "rabbits", "women"
+
+    Parameter
+        quantity: an integer that determines if
+            the returned noun is single or plural.
+    Return: a randomly chosen noun.
+    """
+
+def get_verb(quantity, tense):
+     """Return a randomly chosen verb. If tense is "past",
+    this function will return one of these ten verbs:
+        "drank", "ate", "grew", "laughed", "thought",
+        "ran", "slept", "talked", "walked", "wrote"
+    If tense is "present" and quantity is 1, this
+    function will return one of these ten verbs:
+        "drinks", "eats", "grows", "laughs", "thinks",
+        "runs", "sleeps", "talks", "walks", "writes"
+    If tense is "present" and quantity is NOT 1,
+    this function will return one of these ten verbs:
+        "drink", "eat", "grow", "laugh", "think",
+        "run", "sleep", "talk", "walk", "write"
+    If tense is "future", this function will return one of
+    these ten verbs:
+        "will drink", "will eat", "will grow", "will laugh",
+        "will think", "will run", "will sleep", "will talk",
+        "will walk", "will write"
+
+    Parameters
+        quantity: an integer that determines if the
+            returned verb is single or plural.
+        tense: a string that determines the verb conjugation,
+            either "past", "present" or "future".
+    Return: a randomly chosen verb.
+    """
+def test_get_determiner():
+    # 1. Test the single determiners.
+
+    single_determiners = ["a", "one", "the"]
+
+    # This loop will repeat the statements inside it 4 times.
+    # If a loop's counting variable is not used inside the
+    # body of the loop, many programmers will use underscore
+    # (_) as the variable name for the counting variable.
+    for _ in range(4):
+
+        # Call the get_determiner function which
+        # should return a single determiner.
+        word = get_determiner(1)
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the single_determiners list.
+        assert word in single_determiners
+
+    # 2. Test the plural determiners.
+
+    plural_determiners = ["two", "some", "many", "the"]
+
+    # This loop will repeat the statements inside it 4 times.
+    for _ in range(4):
+
+        # Call the get_determiner function which
+        # should return a plural determiner.
+        word = get_determiner(2)
+
+        # Verify that the word returned from get_determiner
+        # is one of the words in the plural_determiners list.
+        assert word in plural_determiners
+    # Call the get_determiner function.
+    determ = get_determiner(1)
+
+    # Verify that the word stored in the variable
+    # determ is in the list of single determiners.
+    words = ["a", "one", "the"]
+    assert determ in words   
+
+ # Randomly choose and return a determiner.
+    word = random.choice(words)
+    return word
+
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
+  
